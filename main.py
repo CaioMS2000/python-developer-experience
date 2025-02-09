49c049b1a24383215150e35f70d5e0817125f3f7
Caio <caiocpe@gmail.com>
2025-02-09 15:14:35 -0300

    foi o formatador que mudou

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-ABC = "fgionweroig" + "sgfpoijwepg" + "lçrmgpçer´g" + "45656e56sf56ee6" + "4r1g45we4r53qrghjhh  " + "66k90pólpik0-" + "5tyh90&%¨%MKO" + "r5gr85/*/ret4h45hjhn      "
+ABC = (
+    "fgionweroig"
+    + "sgfpoijwepg"
+    + "lçrmgpçer´g"
+    + "45656e56sf56ee6"
+    + "4r1g45we4r53qrghjhh  "
+    + "66k90pólpik0-"
+    + "5tyh90&%¨%MKO"
+    + "r5gr85/*/ret4h45hjhn      "
+)
